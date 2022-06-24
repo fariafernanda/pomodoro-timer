49b05dcb72474f695a3d4bcacd255cc4793c014c
Fernanda <fernandafaria.bio@gmail.com>
2022-06-24 18:17:48 -0300

    add: função startBreakInterval

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 let minutes = 25;
 let seconds = 0;
 
-
-
-function setTimer(){
+function setTimer() {
     minutes < 10 ? minutes = '0' + minutes : minutes;
     seconds < 10 ? seconds = '0' + seconds : seconds;
 
-    document.getElementById('minutes').innerText = minutes;
-    document.getElementById('seconds').innerText = seconds;
+    document.getElementById('minutes').innerHTML = minutes;
+    document.getElementById('seconds').innerHTML = seconds;
 
 }
 
-
-function startTimer(){
-    
+// Tempo de atividade
+function startTimer() {
 
     minutes = minutes - 1;
     seconds = 59;
@@ -25,39 +22,92 @@ function startTimer(){
     let minutesInterval = setInterval(minutesCountdown, 60000);
     let secondsInterval = setInterval(secondsCountdown, 1000);
 
-    function minutesCountdown(){
+    function minutesCountdown() {
         minutes = minutes - 1;
+
         if (minutes < 10) {
             document.getElementById('minutes').innerHTML = '0' + minutes;
         } else {
             document.getElementById('minutes').innerHTML = minutes;
         }
-        
-    
+
     };
 
-    function secondsCountdown(){
+    function secondsCountdown() {
         seconds = seconds - 1;
 
-        if(seconds < 10){
+        if (seconds < 10) {
             document.getElementById('seconds').innerHTML = '0' + seconds;
         } else {
             document.getElementById('seconds').innerHTML = seconds;
         }
-        
-       
-        if(seconds <= 0){
-            if(minutes <= 0){
+
+
+        if (seconds <= 0) {
+            if (minutes <= 0) {
                 clearInterval(minutesInterval);
                 clearInterval(secondsInterval);
 
                 // audio
-                // Começar intervalo
+               startBreakInterval();
 
             }
-
             seconds = 60;
-        } 
-
+        }
     };
+}
+
+
+let breakMinutes = 5;
+let breakSeconds = 0;
+
+// Tempo de descanso
+function startBreakInterval() {    
+
+    breakMinutes = breakMinutes - 1;
+    breakSeconds = 59;
+
+    if (breakMinutes < 10) {
+        document.getElementById('minutes').innerHTML = '0' + breakMinutes;
+    } else {
+        document.getElementById('minutes').innerHTML = breakMinutes;
+    }
+
+    document.getElementById('seconds').innerHTML = breakSeconds;
+
+    let minutesInterval = setInterval(minutesCountdown, 60000);
+    let secondsInterval = setInterval(secondsCountdown, 1000);
+
+    function minutesCountdown() {
+        breakMinutes = breakMinutes - 1;
+        if (breakMinutes < 10) {
+            document.getElementById('minutes').innerHTML = '0' + breakMinutes;
+        } else {
+            document.getElementById('minutes').innerHTML = breakMinutes;
+        }
+    };
+
+    function secondsCountdown() {
+        breakSeconds = breakSeconds - 1;
+
+        if (breakSeconds < 10) {
+            document.getElementById('seconds').innerHTML = '0' + breakSeconds;
+        } else {
+            document.getElementById('seconds').innerHTML = breakSeconds;
+        }
+
+
+        if (breakSeconds <= 0) {
+            if (breakMinutes <= 0) {
+                clearInterval(minutesInterval);
+                clearInterval(secondsInterval);
+
+                // audio
+                startTimer();
+
+            }
+            breakSeconds = 60;
+        }
+    };
+
 }
